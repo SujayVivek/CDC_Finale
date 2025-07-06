e5cda4abe5f3e12cd094d6c03d54570b39f90bf2
SujayVivek <sujayv2016@gmail.com>
2025-07-06 11:34:48 +0530

    15

--- Done/Array_Description.cpp
+++ Done/Array_Description.cpp
@@ -1,61 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define mod 1000000007
+#define ff first
+#define ss second
 typedef vector<vector<long long>> vvi;
 typedef vector<long long> vi;
 #define int long long
 #define endl "\n"
 
-int n, m;
-vi num;
-vvi dp;
+void Solve() {
+    int n, m; cin>>n>>m;
+    vi x(n);
+    for(int i = 0; i<n; i++) cin>>x[i];
 
-int calc(int i, int last) {
-    if (i == n) return 1;
-
-    int &res = dp[i][last];
-    if (res != -1) return res;
-    long long ways = 0;
-
-    if (num[i] == 0) {
-        for (int d = -1; d <= 1; d++) {
-            int cur = last + d;
-            if (cur >= 1 && cur <= m) {
-                ways = (ways + calc(i + 1, cur)) % mod;
+    vvi dp(n+1, vi(m+2, 0));
+    if(x[0] == 0){
+        for(int i = 1; i<=m; i++){
+            dp[0][i] = 1;
+        }
+    }
+    else dp[0][x[0]] = 1;
+    
+    for(int i = 1; i<n; i++){
+        for(int j = 1; j<=m; j++){
+            if(x[i]==0 || x[i]==j){
+                dp[i][j] = (dp[i-1][j] + dp[i-1][j-1] + dp[i-1][j+1])%mod;
             }
         }
-    } else {
-        int cur = num[i];
-        if (abs(cur - last) <= 1) {
-            ways = calc(i + 1, cur);
-        }
     }
-    return res = ways;
-}
-
-void Solve() {
-    cin >> n >> m;
-    num.assign(n, 0);
-    for (int i = 0; i < n; i++) cin >> num[i];
-    dp.assign(n, vi(m+1, -1));
-
-    long long answer = 0;
-
-    if (num[0] == 0) {
-        for (int start = 1; start <= m; start++) {
-            answer = (answer + calc(1, start)) % mod;
-        }
-    } else {
-        answer = calc(1, num[0]);
-    }
-    
-    cout << answer << endl;
+    int ans = 0;
+    for(int i = 0; i<=m; i++) ans= (ans + dp[n-1][i])%mod;
+    cout<<ans<<endl;
 }
 
 int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    Solve();
+    int tt_ = 1;
+    // cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
     return 0;
 }
